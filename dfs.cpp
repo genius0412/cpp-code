ab133c4122182ca8d4cb91c5050d11514d464953
genius <62144272+genius0412@users.noreply.github.com>
2020-08-13 16:10:42 +0900

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define M 100
+
+using namespace std;
+
+vector <int> A[M+5];
+int vt[M+5], v, l;
+
+void input(){
+    scanf("%d%d", &v, &l);
+    int a, b;
+    for(int i=1; i<=l; i++){
+        scanf("%d%d", &a, &b);
+        A[a].push_back(b);
+        A[b].push_back(a);
+    }
+}
+void dfs(int node){
+    printf("%d ", node);
+    vt[node] = 1;
+
+    for(auto u: A[node]){
+        if(vt[u]) continue;
+        dfs(u);
+    }
+}
+int main(){
+    input();
+    dfs(1);
+    return 0;
+}
